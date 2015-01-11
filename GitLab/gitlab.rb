85a2210da0b895ddf85ac6a1a4b928692b5a1ddd
James Harton <james@resistor.io>
2015-01-11 20:52:27 +1300

    Add GitLab.

---
+++ GitLab/gitlab.rb
@@ -0,0 +1,9 @@
+external_url 'http://gitlab.rc'
+
+gitlab_rails['time_zone'] = 'Pacific/Auckland'
+gitlab_rails['gitlab_default_can_create_group'] = true
+gitlab_rails['gitlab_username_changing_enabled'] = true
+gitlab_rails['gitlab_signup_enabled'] = true
+gitlab_rails['gitlab_signin_enabled'] = true
+gitlab_rails['gravatar_enabled'] = false
+
